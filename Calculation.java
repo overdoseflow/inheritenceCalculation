2b6c4788ad325f6b1819751fba079cf707c34d8a
Dumbo <45797944+overdoseflow@users.noreply.github.com>
2018-12-12 00:59:13 +0300

    Create Calculation.java

---
+++ Calculation.java
@@ -0,0 +1,16 @@
+package inheritence;
+
+public class Calculation {
+    
+    int z;
+    public void addition(int x, int y){
+    
+    z = x+y;
+    System.out.println("Sum of numbers: "+z);
+    }
+    public void substruction(int x, int y){
+    z = x-y;
+    System.out.println("Divide of numbers: "+z);
+    
+    }
+}
